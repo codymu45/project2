956ecd7b48b15cf6fef086efd4cde95fcb39cd2b
Cody Mullen <mullen.cody@yahoo.com>
2020-11-14 12:40:08 -0800

    fixed routing issues

--- server.js
+++ server.js
@@ -8,11 +8,18 @@ const passport = require("./config/passport");
 const PORT = process.env.PORT || 8080;
 const db = require("./models");
 
+// Set Handlebars.
+var exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
 // Creating express app and configuring middleware needed for authentication
 const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
+
 // We need to use sessions to keep track of our user's login status
 app.use(
   session({ secret: "keyboard cat", resave: true, saveUninitialized: true })
@@ -34,3 +41,4 @@ db.sequelize.sync().then(() => {
     );
   });
 });
+
