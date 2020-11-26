38a5d425f43af6d59ad14d2bbf4944cda746eece
Cody Mullen <mullen.cody@yahoo.com>
2020-11-25 22:26:24 -0800

    testing html routes

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -24,11 +24,11 @@ module.exports = function(app) {
     res.render("login");
   });
 
-  app.get("/login", (req, res) => {
-    // If the user already has an account send them to the members page
-    res.sendFile(path.join(__dirname, "../public/login.html"));
-    // res.render("root");
-  });
+  // app.get("/login", (req, res) => {
+  //   // If the user already has an account send them to the members page
+  //   res.sendFile(path.join(__dirname, "../public/login.html"));
+  //   // res.render("root");
+  // });
   // Here we've add our isAuthenticated middleware to this route.
   // If a user who is not logged in tries to access this route they will be redirected to the signup page
   app.get("/user", isAuthenticated, (req, res) => {
